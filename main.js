a77d70b4ca4ecf0680f378d5d07c41037ad8af78
Aung Phyo <aungphyo.kh26@gmail.com>
2022-09-28 12:57:22 +0630

    Modal Exercise

--- main.js
+++ main.js
@@ -1,32 +1,21 @@
-Vue.component('message',{
-    props: ['title', 'body'],
-    data(){
-        return {
-            isVisible: true
-        };
-    },
-    template: `
-    <article class="message" v-show="isVisible">
-    <div class="message-header">
-      {{ title }}
-      <button type="button" @click="isVisible = false">x</button> 
+Vue.component('modal',{
+  template: `
+    <div class="modal is-active">
+    <div class="modal-background"></div>
+    <div class="modal-content">
+        <div class="box">
+            <slot></slot>
+        </div>
     </div>
-    <div class="message-body">
-      {{ body }}
-    </div>
-  </article>
-    `,
-
-    
-
-    // methods: {
-    //     hideModal(){
-    //         this.isVisible = false; //Can use directly  in  @click 
-    //     }
-    // }
+    <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+  </div>
+  `
 })
 
 
 new Vue({
-    el: "#root"
+    el: "#root",
+    data:{
+      showModal:false
+    }
 });
